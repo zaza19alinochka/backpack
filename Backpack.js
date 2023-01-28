e85db8f738a05dd213b613509bfe9cc7deddb936
Алина <118599262+zaza19alinochka@users.noreply.github.com>
2023-01-28 14:05:05 +0700

    Исправление

--- Backpack.js
+++ Backpack.js
@@ -1,41 +1,116 @@
-function getMaxOfArray(numArray) {
-    return Math.max.apply(null, numArray);
-  }
-const names = ['часы', 'картина', 'радио', 'ваза', 'книга', 'компьютер', 'золото']
-const prices = [175, 90, 20, 50, 10, 200, 3000]
-const weights = [10, 9, 4, 2, 1, 20, 1]
-const items = []
-const maxWeights = 20
-let myItems = []
-let nowWeights = 0
-let allowedWeight = maxWeights
-let maxPrice = getMaxOfArray(prices)
-for (let i = 0; i < prices.length; i++)
+function buildItems()
 {
-    items.push({
-        name:names[i], 
-	    price: prices[i], 
-	    weight: weights[i]
-    })
+	const names = ['часы', 'картина', 'радио', 'ваза', 'книга', 'компьютер']
+	const values = [175, 90, 20, 50, 10, 200]
+	const weights = [10, 9, 4, 2, 1, 20]
+	const items = []
+	for (let i = 0; i < values.length; i++)
+	{
+		items.push({
+			name:names[i], 
+			value: values[i], 
+			weight: weights[i]
+		})
+	} 
+	return items
 }
-console.log(items)
-while(allowedWeight > 0 ) {
-    for (let k = 0; k < items.length; k++) {
-        if(items[k].weight > allowedWeight) {
-            items.splice(k, 1)
-            prices.splice(k, 1)
-            maxPrice = getMaxOfArray(prices)
-            k = 0
-        }
-        else if (items[k].price == maxPrice && items[k].weight <= allowedWeight){
-            myItems.push(items[k])
-            nowWeights += items[k].weight
-            allowedWeight -= items[k].weight
-            console.log(allowedWeight)
-            items.splice(k, 1)
-            prices.splice(k, 1)
-            maxPrice = getMaxOfArray(prices)
-        }
-    }
-}   
-console.log(myItems)
+const items = buildItems()
+
+
+function* genBin(n)
+{
+	let b = []
+	for(let i = 0; i < n; i++)
+		b.push(0)
+	yield b
+	let i = 0
+	while(true)
+	{
+		if(i == n)
+		{
+			return
+		}
+		else if( b[i] == 1)
+		{
+			b[i] = 0
+			i++
+		}
+		else
+		{
+			b[i] = 1
+			i = 0
+			yield b
+		}
+	}
+}
+	let res = genSolution(weightFilter(20, genPowerSet(items)))
+	console.log(res.totalValue)
+	console.log(res.taken)
+
+
+
+function* genPowerSet(items)
+{
+	for(let x of genBin(items.length))
+	{
+		let taken = []
+		for (let i = 0; i < x.length; i++)
+		{
+			if (x[i] == 1) taken.push(items[i])
+		}
+		yield taken
+	}
+}
+
+
+function weightTest(taken, maxWeight)
+{
+	totalWeight = 0
+	for(let t of taken)
+	{
+		totalWeight += t.weight 
+		if(totalWeight > maxWeight)
+		{
+			return false
+		}
+	}
+	return true
+}
+
+
+
+function* weightFilter(maxWeight, genPowerSet)
+{
+	for(let taken of genPowerSet)
+	{
+		if (weightTest(taken, maxWeight))
+			yield taken
+	}
+}
+
+
+
+function genSolution(xF)
+{
+	let totalValue = 0
+	let taken = []
+	for (let x of xF)
+	{
+		if (value(x) > totalValue)
+		{
+			totalValue = value(x)
+			taken = x.slice()
+		}
+	}
+	return {"totalValue": totalValue, "taken": taken}
+}
+
+function value(taken)
+{
+	let totalValue = 0
+	for(let item of taken)
+	{
+		totalValue += item.value 
+	}
+	return totalValue
+}
